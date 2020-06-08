f3356c7194d779a16439b38a3aabbf6f4d707319
Umar Haroon <umar.m.haroon@gmail.com>
2020-06-08 14:16:07 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const rateLimit = require('express-rate-limit');
+const { body, check } = require('express-validator');
+const cors = require('cors')
+const express = require('express')
+const helmet = require('helmet');
+const { pool } = require('./config')
+
+const app = express()
+
+app.use(compression())
+app.use(helmet())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// const isProduction = process.env.NODE_ENV === 'production'
+// const origin = {
+//     orign: isProduction ? 'https://www.example.com' : '*',
+// }
+app.use(cors())
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 10,
+})
+
+app.use(limiter)
+
+const getErrors = (request, response) => {
+    pool.query('SELECT errorID, message FROM harbor', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const addError = (request, response) => {
+    const { errorid, message } = request.body[0]
+    console.log(request.body[0])
+    pool.query('INSERT INTO harbor (errorID, message) VALUES ($1, $2)', [errorid, message], error => {
+        if (error) {
+            throw error
+        }
+        response.status(201).json({ status: 'success', message: 'Error added.' })
+    })
+}
+
+app
+    .route('/')
+    // GET endpoint
+    .get(getErrors)
+    // POST endpoint
+    .post(addError, [check('errorID').not().isEmpty().isLength({ min: 5, max: 255 }).trim(), check('longMessage').not().isEmpty().trim()])
+
+// Start server
+app.listen(process.env.PORT || 3002, () => {
+    console.log(`Server listening`)
+})
